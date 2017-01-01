cd0c4ecb617559a419f4c88585c21f161669d73d
DeValladolid <devalladolidoficial@gmail.com>
2017-01-01 15:36:16 +0100

    Change many packages

---
+++ app/src/main/java/com/devalladolid/musictoday/widgets/desktop/WhiteWidget.java
@@ -0,0 +1,15 @@
+package com.devalladolid.musictoday.widgets.desktop;
+
+import com.devalladolid.musictoday.R;
+
+/**
+ * Created by nv95 on 11.11.16.
+ */
+
+public class WhiteWidget extends StandardWidget {
+
+    @Override
+    int getLayoutRes() {
+        return R.layout.widget_white;
+    }
+}
